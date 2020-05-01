3c4729c1ec0ec8aec5e932373616cb624d08be46
CodeforEvolution <secundaja@gmail.com>
2020-04-30 22:07:10 -0500

    Haiku: Finish the MIDIDriver class
    Now...just to test it...

--- platform/haiku/midi_driver_midi2_kit.cpp
+++ platform/haiku/midi_driver_midi2_kit.cpp
@@ -34,104 +34,91 @@
 
 #include "core/print_string.h"
 
-#include <MidiConsumer.h>
 #include <MidiProducer.h>
 #include <MidiRoster.h>
 
+class GodotMidiLocalConsumer : public BMidiLocalConsumer {
+	
+	MIDIDriverMIDI2Kit *midi_driver
+	
+public:
+	GodotMidiLocalConsumer(const char *name, MIDIDriverMIDI2Kit *p_midi_driver) :
+			BMidiLocalConsumer(name),
+			midi_driver(p_midi_driver) {
+	};
+	
+	void Data(uchar *data, size_t length, bool atomic, bigtime_t time) {
+
+		if (atomic) {
+			midi_driver->receive_input_packet(time, data, length);
+		}
+	};
+};
+
 Error MIDIDriverMIDI2Kit::open() {
-
-	CFStringRef name = CFStringCreateWithCString(NULL, "Godot", kCFStringEncodingASCII);
-	OSStatus result = MIDIClientCreate(name, NULL, NULL, &client);
-	CFRelease(name);
-	if (result != noErr) {
-		ERR_PRINTS("MIDIClientCreate failed, code: " + itos(result));
+	
+	input_consumer = new GodotMidiLocalConsumer("Godot Input", this);
+	if (input_consumer == NULL) {
+		ERR_PRINTS("GodotMidiLocalConsumer construction failed");
+		return ERR_CANT_OPEN;
+	}
+	
+	if (!input_consumer->IsValid()) {
+		ERR_PRINTS("GodotMidiLocalConsumer failed to start up");
+		input_consumer->Release();
+		return ERR_CANT_OPEN;
+	}
+	
+	if (input_consumer->Register() != B_OK) {
+		ERR_PRINTS("GodotMidiLocalConsumer failed to register itself with BMidiRoster");
+		input_consumer->Release();
 		return ERR_CANT_OPEN;
 	}
 
-	result = MIDIInputPortCreate(client, CFSTR("Godot Input"), MIDIDriverCoreMidi::read, (void *)this, &port_in);
-	if (result != noErr) {
-		ERR_PRINTS("MIDIInputPortCreate failed, code: " + itos(result));
-		return ERR_CANT_OPEN;
-	}
-
-	int sources = MIDIGetNumberOfSources();
-	for (int i = 0; i < sources; i++) {
-
-		MIDIEndpointRef source = MIDIGetSource(i);
-		if (source) {
-			MIDIPortConnectSource(port_in, source, (void *)this);
-			connected_sources.insert(i, source);
+	bool more_producers = true;
+	int32 producer_id = 0;
+	while (more_producers) {
+		BMidiProducer *producer = BMidiRoster::NextProducer(&producer_id);
+		if (producer != NULL) {
+			if (producer->Connect(input_consumer) == B_OK) {
+				connected_sources.push_back(producer_id);
+			}
+		} else {
+			more_producers = false;
 		}
 	}
-
+	
 	return OK;
-	
-	input_consumer = new MidiInternalLocalConsumer("Godot");
-	if (input_consumer == NULL) {
-		ERR_PRINTS("MidiInternalLocalConsumer construction failed");
-		return ERR_CANT_OPEN;
-	}
-	
-	
 }
 
 void MIDIDriverMIDI2Kit::close() {
 
 	for (int index = 0; index < connected_sources.size(); index++) {
 		int32 producer_id = connected_sources[index];
-		MIDIPortDisconnectSource(port_in, source);
+		BMidiProducer *producer = BMidiRoster::FindProducer(producer_id);
+		if (producer != NULL) {
+			producer->Disconnect(input_consumer);
+			producer->Release();
+		}	
 	}
 	connected_sources.clear();
 
-	if (port_in != 0) {
-		MIDIPortDispose(port_in);
-		port_in = 0;
-	}
-
-	if (client != 0) {
-		MIDIClientDispose(client);
-		client = 0;
-	}
+	input_consumer->Unregister();
+	input_consumer->Release();
 }
 
 PoolStringArray MIDIDriverMIDI2Kit::get_connected_inputs() {
 
 	PoolStringArray list;
-
-	for (int i = 0; i < connected_sources.size(); i++) {
-		MIDIEndpointRef source = connected_sources[i];
-		CFStringRef ref = NULL;
-		char name[256];
-
-		MIDIObjectGetStringProperty(source, kMIDIPropertyDisplayName, &ref);
-		CFStringGetCString(ref, name, sizeof(name), kCFStringEncodingUTF8);
-		CFRelease(ref);
-
-		list.push_back(name);
-	}
-
-	return list;
-
-	bool more_producers = true;
-	int32 id = 0;
-	while (more_producers) {
-		BMidiProducer *producer = BMidiRoster::NextProducer(&id);
-		if (producer != NULL) {
-			list.push_back(producer->Name());
-			producer->Release();
-		} else {
-			more_producers = false;
-		}
-	}
-
-	return list;
-	
-	PoolStringArray list;
-	
 	for (int index = 0; index < connected_sources.size(); index++) {
 		int32 producer_id = connected_sources[index];
-		BMidiProducer *producer 
+		BMidiProducer *producer = BMidiRoster::FindProducer(producer_id);
+		if (producer != NULL) {
+			list.push_back(producer->Name());
+		}
 	}
+	
+	return list;
 }
 
 MIDIDriverMIDI2Kit::MIDIDriverMIDI2Kit() :
